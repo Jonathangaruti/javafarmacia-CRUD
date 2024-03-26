06c532aff792c7bcf39518db57eb2cac108a38e5
Jonathan Garuti <jonathan_garuti@hotmail.com>
2024-03-26 09:55:28 -0300

    CRUD Farmacia

---
+++ src/main/java/com/generation/javafarmacia/JavaFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.javafarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFarmaciaApplication.class, args);
+	}
+
+}
