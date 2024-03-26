323843829360051c77e9c694535148bb820bf511
Jonathan Garuti <jonathan_garuti@hotmail.com>
2024-03-26 14:54:31 -0300

    Configuração do ambiente para Teste de Software com JUnit 5.

---
+++ src/main/java/com/generation/javafarmacia/configuration/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package com.generation.javafarmacia.configuration;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+    OpenAPI springBlogPessoalOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Projeto Java Farmacia - Generation Brasil")
+                .description("Projeto Java Farmacia")
+                .version("v0.0.1")
+                .license(new License()
+                    .name("Jonathan Rocha")
+                    .url("https://jonathangaruti.github.io/"))
+                .contact(new Contact()
+                    .name("Jonathan Rocha")
+                    .url("https://github.com/Jonathangaruti")
+                    .email("jonathan_garuti@hotmail.com")))
+            .externalDocs(new ExternalDocumentation()
+                .description("Github")
+                .url("https://github.com/Jonathangaruti/javafarmacia-CRUD"));
+    }
+
+
+	@Bean
+	OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
+
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+
+				ApiResponses apiResponses = operation.getResponses();
+
+				apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+				apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+				apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+				apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+				apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+				apiResponses.addApiResponse("403", createApiResponse("Acesso Proibido!"));
+				apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
+				apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+
+			}));
+		};
+	}
+
+	private ApiResponse createApiResponse(String message) {
+
+		return new ApiResponse().description(message);
+
+	}
+}
